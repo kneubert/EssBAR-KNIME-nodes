1be63e66fb4086df5fd27d8a9b0ad2cfe50d13ba
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-06-27 17:19:59 +0200

    SPAdes node changed to original code, removed IO nodes (could not include required classes) and all QC nodes (could not include required classes)

--- de.essbar.ngs_toolbox.QC.linux.x86_64/payload/bin/multiqc.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wrapper script to MultiQC that accepts the same arguments
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH="${DIR}/lib/python2.7/site-packages"
-
-echo "$DIR/multiqc $*"
-$DIR/multiqc $*
