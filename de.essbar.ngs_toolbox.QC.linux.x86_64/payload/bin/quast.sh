192f288823b8d2904f9b1caab8b623559bfc8861
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-06-27 17:19:59 +0200

    SPAdes node changed to original code, removed IO nodes (could not include required classes) and all QC nodes (could not include required classes)

--- de.essbar.ngs_toolbox.QC.linux.x86_64/payload/bin/quast.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Wrapper script to QUAST that accepts the same arguments
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH=${DIR}/quast_lib:${PYTHONPATH}
-
-echo "$DIR/quast.py $*"
-$DIR/quast.py $*
