f7dfc6f6e500768ca0f6efd26cd5590b2cc75962
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-07-16 00:58:06 +0200

    SRST2 node

---
+++ de.essbar.ngs_toolbox.apps/src/de/essbar/ngs_toolbox/apps/knime/nodes/SRST2/SRST2NodeModel.java
@@ -0,0 +1,18 @@
+// THIS CODE WAS GENERATED WITH THE GENERIC WORKFLOW NODES FOR KNIME NODE GENERATOR
+// DO NOT MODIFY
+package de.essbar.ngs_toolbox.apps.knime.nodes.SRST2;
+
+import com.genericworkflownodes.knime.generic_node.GenericKnimeNodeModel;
+
+import com.genericworkflownodes.knime.config.INodeConfiguration;
+import com.genericworkflownodes.knime.custom.config.IPluginConfiguration;
+
+/**
+    @brief SRST2 Node Model.
+*/
+public class SRST2NodeModel extends GenericKnimeNodeModel {  
+    protected SRST2NodeModel(INodeConfiguration nodeConfig,
+            IPluginConfiguration pluginConfig) {
+        super(nodeConfig, pluginConfig, new String[][]{{"fastq" ,"fq" ,"gz" }, {"fastq" ,"fq" ,"gz" }, {"fasta" ,"fa" }, {"txt" }, {"fasta" ,"fa" },}, new String[][]{{"*.*", "Inactive" },});
+    }
+}
