26fc065eb69b88c05a9ba865f533aea1d07ded64
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-08-07 17:52:44 +0200

    correct contigMLST script to remove overwritten blast parameter

--- de.essbar.ngs_toolbox.apps.linux.x86_64/payload/bin/contigMLST.sh
+++ de.essbar.ngs_toolbox.apps.linux.x86_64/payload/bin/contigMLST.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Wrapper script to mlst that accepts the same arguments except
-# for the database (uses the folder).
-# first, parse the options:
+# Wrapper script to mlst that accepts the same arguments except for the blast database 
+# (overwrite parameter to use the mlst.fa file for the blast db inside the given folder)
 
-blastfile=""
 datadir=""
+blastfile=""
+blastval=""
+novelfile=""
+
 # Get path to output file and shift it.
 OUT=$1
 shift
@@ -15,20 +17,25 @@ while getopts $optspec optchar; do
     case "${optchar}" in
         -)
             case "${OPTARG}" in
-                datadir)
+				datadir)
                     val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
 		    		echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
 		    		datadir=${val}; 
-                    ;;
+                    ;;	
                 blastdb)
                     val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
 		    		echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
+					blastval=${val}
 		    		blastfile=${val}"/mlst.fa"; 
-                    ;;
+                    ;;		
+				novel)
+                    val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
+		    		echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
+					novelfile=${val}
+                    ;;			
                 *)
                     val="${!OPTIND}"; OPTIND=$(( $OPTIND + 1 ))
-		    echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
-                    
+		    		echo "Parsing option: '--${OPTARG}', value: '${val}'" >&2;
             esac;;
         *)
             if [ "$OPTERR" != 1 ] || [ "${optspec:0:1}" = ":" ]; then
@@ -38,26 +45,40 @@ while getopts $optspec optchar; do
     esac
 done
 
-shift $((OPTIND - 2))
+echo $blastval
+# remove --blastdb parameter and value
+for arg do 
+   shift
+   [ "$arg" = "--blastdb" ] || [ "$arg" = "${blastval}" ]  && continue
+   set -- "$@" "$arg"
+done
 
+
+# check if fasta file mlst.fa for blast db exists
 if [[ ! -e $blastfile ]];
 then
    echo ERROR blast file $blastfile does not exist 1>&2
    exit 1
 fi
 
+# check if folder for mlst database exists
 if [[ ! -e $datadir ]];
 then
-   echo ERROR mlst database directory $datatdir does not exist 1>&2
+   echo ERROR mlst database directory $datadir does not exist 1>&2
    exit 1
 fi
-
-echo $blastfile
 
 # THEN, access the positional params
 echo "there are $# positional params remaining"
 for ((i=1; i<=$#; i++)); do
   printf "%d\t%s\n" $i "${!i}"
 done
-echo "mlst --blastdb $blastfile --datadir $datadir $* >$OUT"
-mlst --blastdb $blastfile --datadir $datadir $* >$OUT
+
+echo "mlst --blastdb $blastfile $* >$OUT"
+mlst --blastdb $blastfile $* >$OUT
+
+# create empty file for novel alleles
+if [[ ! -e $novelfile ]];
+then 
+  echo "Found no novel intact alleles." >> $novelfile
+fi
