1a8ddccb6ba4cfb48a45b67d956820cf515cec05
Kerstin Neubert <kerstin.neubert@fu-berlin.de>
2019-10-10 15:36:20 +0200

    bug fix multiqc

--- de.essbar.ngs_toolbox.QC.linux.x86_64/payload/bin/multiqc.sh
+++ de.essbar.ngs_toolbox.QC.linux.x86_64/payload/bin/multiqc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Wrapper script to MultiQC that accepts the same arguments
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-export PYTHONPATH="${DIR}/lib/python2.7/site-packages"
+export PYTHONPATH="${DIR}/lib/python2.7/site-packages:$PYTHONPATH"
 
 echo "$DIR/multiqc $*"
 $DIR/multiqc $*
